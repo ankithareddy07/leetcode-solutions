dde2252bf2ebe3778b0452e5dae58dd7dd5cb9d2
ankithareddy07 <premaankitha07@gmail.com>
2026-04-18 23:00:09 +0530

    Add solution for minimum bit flips problem

---
+++ 2220-Minimum Bit flips to convert number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        ans = start^goal
+        c = 0
+        while(ans!=0):
+            rem = ans%2
+            if rem==1:
+                c+=1
+            ans = ans//2
+        return c
